0d8181cf4b0623fffe9d530451ad655c0ad6b77e
d.rizun <denishywde@gmail.com>
2025-06-08 21:39:57 +0200

    [base]: added settings, db-setup, requirements and extra base infrastrure

---
+++ config.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings
+
+BASE_DIR: Path = Path(__file__).resolve().parent
+
+
+class SettingsConfig(BaseSettings):
+    CORS_ORIGINS: list[str] = ["*"]
+
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    INNER_POSTGRES_PORT: int
+    OUTER_POSTGRES_PORT: int
+
+    @property
+    def database_connection(self) -> str:
+        return (
+            f"postgresql+asyncpg://"
+            f"{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+            f"@{self.POSTGRES_HOST}:{self.INNER_POSTGRES_PORT}"
+            f"/{self.POSTGRES_DB}"
+        )
+
+    class Config:
+        env_file = BASE_DIR / ".env"
+        extra = "ignore"
+
+
+config: SettingsConfig = SettingsConfig()
---
+++ db_setup.py
@@ -0,0 +1,39 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine
+)
+
+from config import config
+
+
+class DatabaseSetup:
+
+    @classmethod
+    def setup(
+            cls,
+            echo: bool = False,
+            expire: bool = False
+    ) -> async_sessionmaker[AsyncSession]:
+        engine: AsyncEngine = cls._create_engine(echo=echo)
+        return cls._create_session_pool(engine=engine, expire=expire)
+
+    @classmethod
+    def _create_engine(cls, echo: bool = False) -> AsyncEngine:
+        return create_async_engine(
+            url=config.database_connection,
+            query_cache_size=1200,
+            pool_size=20,
+            max_overflow=200,
+            future=True,
+            echo=echo,
+        )
+
+    @classmethod
+    def _create_session_pool(
+            cls,
+            engine: AsyncEngine,
+            expire: bool = False
+    ) -> async_sessionmaker[AsyncSession]:
+        return async_sessionmaker(bind=engine, expire_on_commit=expire)
